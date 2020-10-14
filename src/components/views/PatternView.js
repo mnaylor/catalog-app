2f56b2b4ee6f6a5616851bd652ce15d8557b295a
Michelle Naylor <michnaylor@gmail.com>
2020-10-14 11:44:26 -0600

    Added simple pattern view

--- src/components/views/PatternView.js
+++ src/components/views/PatternView.js
@@ -1,10 +1,87 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: "45%",
+      padding: "20px"
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%',
+    },
+    link: {
+      textDecoration: 'none'
+    },
+    chip: {
+        margin: theme.spacing(0.5),
+      }
+  }));
 
 function PatternView(props) {
+    const classes = useStyles();
+
+    const chips = [
+        {
+            label: "top"
+        },
+        {
+            label: "bottom"
+        }
+    ];
+
+    const tags = [
+        {
+            label: "foo"
+        },
+        {
+            label: "bar"
+        }
+    ];
+
+    function getMetadataCard() {
+        return (
+            <Paper className={classes.root}>
+                <Typography className={classes.pos} variant="overline" display="block" gutterBottom>
+                    Pattern Types
+                </Typography>
+                {
+                    chips.map(chip => (<Chip
+                        icon={chip.icon}
+                        label={chip.label}
+                        className={classes.chip}
+                    />)
+                    )
+                }
+
+                <Typography className={classes.pos} variant="overline" display="block" gutterBottom>
+                    Tags
+                </Typography>
+                {
+                    tags.map(chip => (<Chip
+                        icon={chip.icon}
+                        label={chip.label}
+                        className={classes.chip}
+                        />)
+                    )
+                }
+            </Paper>
+        )
+    }
+
     return (
         <div>
-            Pattern View
-            {props.match.params.id}
+            <Typography className={classes.pos} variant="h4">
+                Pattern Name goes here <ExitToAppIcon/>
+            </Typography>
+            <Typography className={classes.pos} variant="h5">
+                Company
+            </Typography>
+            {getMetadataCard()}
         </div>
     )
 }
